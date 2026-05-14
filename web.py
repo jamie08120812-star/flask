8953f5eb1495cd062d016de6cf0c87d08844d007
黃盈箏 <jamie08120812@gmail.com>
2026-05-14 11:32:43 +0800

    Add files via upload

--- web.py
+++ web.py
@@ -55,9 +55,13 @@ def webhook():
     # build a request object
     req = request.get_json(force=True)
     # fetch queryResult from json
-    action =  req.get("queryResult").get("action")
-    msg =  req.get("queryResult").get("queryText")
-    info = "我是黃營箏的機器人動作：" + action + "； 查詢內容：" + msg
+    action =  req["queryResult"]["action"]
+    #msg =  req.get("queryResult").get("queryText")
+    #info = "我是黃營箏的機器人動作：" + action + "； 查詢內容：" + msg
+    if (action == "rateChoice"):
+        rate =  req["queryResult"]["parameters"]["rate"]
+        info = "我是黃營箏的機器人，您選擇的電影分級是：" + rate
+
     return make_response(jsonify({"fulfillmentText": info}))
 
 
